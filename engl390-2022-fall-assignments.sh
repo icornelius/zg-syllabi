6a07b04aa7d36347a1d6327ddee5dfff31c0ef49
Ian Cornelius <icornelius@luc.edu>
2022-11-04 18:41:17 -0500

    390: add prompts for weeks 5-15
    To do:
    
    - revise translation prompts
    - Hamlet Q1/F prompts
    - Final essay prompts (review)

--- engl390-2022-fall-assignments.sh
+++ engl390-2022-fall-assignments.sh
@@ -23,28 +23,95 @@ PANDOC_FRONTMATTER
 echo "\newpage"
 echo
 
-# Writing systems and speech sounds
+# Course introduction: writing systems and speech sounds (week 2)
 heading
-echo "**Prompts for 09-06.**"
-zfind $COMMON --query-string 'tags:"ENGL 390" & tags:"fall 2022" & tags:prompt & tags:week1' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'filename:20220830084038' $PUBLISH  | pandoc_shift_headings
 echo "\newpage"
 echo
 
-# Exploring Middle English words
+# Exploring Middle English words (week 3)
 heading
-echo "**Prompt for 09-13.**"
 zfind $COMMON --query-string 'filename:20210822133004' $PUBLISH  | pandoc_shift_headings
 echo "\newpage"
 echo
 
-# Translation and commentary
+# Translation and commentary (week 4)
 heading
-zfind $COMMON --query-string 'tags:"ENGL 390" & tags:"fall 2022" & tags:prompt & tags:translation' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'filename:20200120155517' $PUBLISH  | pandoc_shift_headings
 echo "\newpage"
 echo
 
-# Rubric for poetry recitation
+# Rubric for poetry recitation (week 4)
 heading
-zfind $COMMON --query-string 'tags:rubric & tags:"poetry recitation" & tags:Chaucer' $PUBLISH  | pandoc_shift_headings
+zfind $COMMON --query-string 'filename:20200116101507' $PUBLISH  | pandoc_shift_headings
 echo "\newpage"
 echo
+
+# Introduction to medieval handwritten books (week 5)
+heading
+zfind $COMMON --query-string 'filename:20221104173823' $PUBLISH  | pandoc_shift_headings
+echo "\newpage"
+echo
+
+# Manuscripts and/of Chaucer's poetry (week 6)
+heading
+zfind $COMMON --query-string 'filename:20221104174814' $PUBLISH  | pandoc_shift_headings
+echo "\newpage"
+echo
+
+# Alisoun of Bath: sources, texts, glosses (week 7)
+heading
+zfind $COMMON --query-string 'filename:20221003204226' $PUBLISH  | pandoc_shift_headings
+echo "\newpage"
+echo
+
+# Midterm essay (week 8)
+heading
+zfind $COMMON --query-string 'filename:20221007183937' $PUBLISH  | pandoc_shift_headings
+echo "\newpage"
+echo
+
+# Introduction to early printed books (week 9)
+heading
+zfind $COMMON --query-string 'filename:20210922000000' $PUBLISH  | pandoc_shift_headings
+echo "\newpage"
+echo
+
+# Encountering *Hamlet* (week 10)
+heading
+zfind $COMMON --query-string 'filename:20221101101723' $PUBLISH  | pandoc_shift_headings
+echo "\newpage"
+echo
+
+# The second quarto *Hamlet* (week 11)
+heading
+echo "Respond to any prompt you did not select in last week's assignment. You may write on material from any part of the first quarto text of *Hamlet*."
+echo "\newpage"
+echo
+
+# Three texts of *Hamlet* (week 12)
+heading
+zfind $COMMON --query-string 'filename:20221104205657' $PUBLISH  | pandoc_shift_headings
+echo "\newpage"
+echo
+
+# Annotated bibliography and presentation (week 13)
+heading
+zfind $COMMON --query-string 'filename:20211110000000' $PUBLISH  | pandoc_shift_headings
+echo "\newpage"
+echo
+
+# Final essay (week 15)
+heading
+echo
+
+## Instructions
+heading
+zfind $COMMON --query-string 'filename:20180321000000' $PUBLISH  | pandoc_shift_headings
+echo
+
+## Prompts
+heading
+zfind $COMMON --query-string 'filename:20221104214813' $PUBLISH  | pandoc_shift_headings
+echo
+
