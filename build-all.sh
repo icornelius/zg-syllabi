0a0187ff4db030c40271e2895990278a7188c33f
Ian Cornelius <icornelius@luc.edu>
2025-08-16 15:04:37 -0500

    update scripts (2/2): update bash scripts
    - set COURSES as a shell array and iterate
    - run date-formatter.py within build-all.sh
    - improve messaging

--- build-all.sh
+++ build-all.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
-COURSE_1=engl320-2025-fall
-#COURSE_2=engl323-2024-fall
+COURSES=("engl320-2025-fall" "engl299-2025-fall")
 
-# Unlock Markdown files for overwriting
+# Convert CSV schedules to Markdown
+pushd schedules
+
+echo "Converting CSV-formatted schedules to Markdown..."
+
+for COURSE in ${COURSES[@]} ; do
+	python3 ../scripts/date-formatter.py $COURSE.csv
+done
+
+popd
+
+# Unlock Markdown files in the build directory
 for f in build/*md ; do chmod 666 $f ; done
 
+# Build a single Markdown file from atomized components
 pushd partials
 
-# Build a single Markdown file from atomized components
-pandoc --metadata-file ../config/pandoc-metadata.yaml --lua-filter include-files.lua --wrap=preserve ../frames/$COURSE_1.md -so ../build/$COURSE_1.md
-# pandoc --metadata-file ../config/pandoc-metadata.yaml --lua-filter include-files.lua --wrap=preserve ../frames/$COURSE_2.md -so ../build/$COURSE_2.md
+echo "Building composite Markdown from components..."
+for COURSE in ${COURSES[@]} ; do
+	pandoc --metadata-file ../config/pandoc-metadata.yaml --lua-filter include-files.lua --wrap=preserve ../frames/$COURSE.md -so ../build/$COURSE.md
+	echo -e "\tCreated /build/$COURSE.md"
+done
 
 popd
 
 # Make Markdown files in build/ read-only
 for f in build/*md ; do chmod 444 $f ; done
 
-# Build the PDF
+# Call build-pdf.sh to build the PDF(s)
 bash build-pdf.sh
+
+echo "Done"
--- build-pdf.sh
+++ build-pdf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-COURSE_1=engl320-2025-fall
-#COURSE_2=engl323-2024-fall
+COURSES=("engl320-2025-fall" "engl299-2025-fall")
 
-echo 'Creating pdf for' $COURSE_1 '...'
-pandoc --citeproc --toc --number-sections --metadata-file config/latex-header-includes.yaml build/$COURSE_1.md -o build/$COURSE_1.pdf
-#echo 'Creating pdf for' $COURSE_2 '...'
-#pandoc --citeproc --toc --number-sections --metadata-file config/latex-header-includes.yaml build/$COURSE_2.md -o build/$COURSE_2.pdf
+for COURSE in ${COURSES[@]} ; do
+        echo "Creating /build/$COURSE.pdf..."
+	pandoc --citeproc --toc --number-sections --metadata-file config/latex-header-includes.yaml build/$COURSE.md -o build/$COURSE.pdf
+	echo -e "\tSuccess"
+done
