13cc5fa1aa6aa0f27c4d6f56958d4acd4610336a
Ian Cornelius <icornelius@luc.edu>
2026-01-09 19:21:32 -0600

    chore (413): update build script

--- build-pdf.sh
+++ build-pdf.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-COURSES=("comp250-engl296")
+COURSES=("engl413")
+NAME="cornelius"
+SEMESTER="2026-01"
 
 for COURSE in ${COURSES[@]} ; do
-        echo "Creating /build/$COURSE.pdf..."
-	pandoc \
-	        --metadata-file config/pandoc-metadata.yaml \
+        echo "Creating /build/$NAME-$COURSE-$SEMESTER.pdf..."
+	pandoc --metadata-file config/pandoc-metadata.yaml \
 		--lua-filter include-files.lua \
 		--citeproc --toc --number-sections \
                 frames/$COURSE.md \
-		-o build/$COURSE.pdf &&
+		-o build/$NAME-$COURSE-$SEMESTER.pdf &&
 		echo -e "\tSuccess"
 done
